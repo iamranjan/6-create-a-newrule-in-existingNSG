bfed0564af70b3b646940fa4280040bd50d21101
iamranjan <ranjan.engnr@gmail.com>
2024-08-27 16:41:19 +0530

    Update backend.tf

--- terraform-manifests/backend.tf
+++ terraform-manifests/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name  = "mytfstate987612345"
     container_name        = "tfstate"
     key                   = "env/nsg/terraform.tfstate"  # This is the name of the state file
+
   }
 }
